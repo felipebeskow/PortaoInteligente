17aa632cc3e94ed6e8f53f4b0ba1777cebb4e9e9
Felipe Beskow <felipebeskow@outlook.com>
2021-05-29 22:25:09 -0300

    Projeto Arduino

---
+++ sketch_abrir_portao/sketch_abrir_portao.ino
@@ -0,0 +1,62 @@
+
+#include <SPI.h> 
+#include <UIPEthernet.h>
+ 
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; 
+byte ip[] = { 192, 168, 2, 175 };
+EthernetServer server(80); 
+
+String readString = String(30);
+int status = 0;
+ 
+int RelayPin = 6;
+
+void setup() {
+    Ethernet.begin(mac, ip); 
+    server.begin();
+    pinMode(RelayPin, OUTPUT);
+    digitalWrite(RelayPin, HIGH);
+    Serial.begin(9600);
+}
+
+
+void abrirPortao(){
+    digitalWrite(RelayPin, LOW);
+    Serial.print("LOW\n");
+    delay(1000);
+
+    digitalWrite(RelayPin, HIGH);
+    Serial.print("HIGH\n");
+}
+
+
+void loop() {
+    EthernetClient client = server.available(); 
+    if (client) {
+        while (client.connected()) {
+            if (client.available()) { 
+                char c = client.read(); 
+                if (readString.length() < 100) 
+                {
+                    readString += c; 
+                }  
+                if (c == '\n') { 
+                    if (readString.indexOf("?") <0){ 
+                    }
+                    else
+                    if(readString.indexOf("abrir=helfensteler") >0){ 
+                        abrirPortao();
+                    }
+                    client.println("HTTP/1.1 200 OK");
+                    client.println("Content-Type: text/html");
+                    client.println();
+                    client.println("<body><html>");
+                    client.println("<center><form method=get name=LED><input type=hidden name=abrir value=\"helfensteler\" /><input type=submit value=Abrir Portão></form></center>");
+                    client.println("</body></html>");
+                    readString=""; 
+                    client.stop(); 
+                }
+            }
+        }
+    }
+}
